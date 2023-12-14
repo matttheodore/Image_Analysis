448622e08b20f4bc9dc7092e3659f808db3ee111
matttheodore <matttheodore@tamu.edu>
2023-12-14 13:17:55 -0600

    The following files

---
+++ Python/functions/list_directory_structure_GPT.py
@@ -0,0 +1,15 @@
+
+import os
+
+def list_files_to_txt(startpath):
+    with open(os.path.join(startpath, "directory_structure.txt"), "w") as txt_file:
+        for root, dirs, files in os.walk(startpath):
+            level = root.replace(startpath, '').count(os.sep)
+            indent = ' ' * 4 * (level)
+            txt_file.write(f"{indent}{os.path.basename(root)}/\n")
+            subindent = ' ' * 4 * (level + 1)
+            for f in files:
+                txt_file.write(f"{subindent}{f}\n")
+
+# Usage example; replace the directory path with the one you want to explore.
+list_files_to_txt(r"F:\Matthew Theodore_Microscopy\RNA Phages\FISH\biorep_1_FISH_test")
